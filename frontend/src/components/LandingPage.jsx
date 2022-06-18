0ddf510f17d0ca54af6158ad7aab3e148c1fa2f6
kelshaaira <kelshairaa@gmail.com>
2022-06-18 21:02:07 +0700

    header fix bismillah
    tinggal di onclick sama redirect

--- frontend/src/components/LandingPage.jsx
+++ frontend/src/components/LandingPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 export default function LandingPage() {
   return (
@@ -6,19 +6,22 @@ export default function LandingPage() {
         <link rel="stylesheet" href='./assets/css/landingPage.css'/>
 
         <div className='header'>
-            <img className='logo' src='./assets/img/logo.png'/>
-            <div className='searchbar'>
-                <input className='search' type="text" placeholder='Search here . . .' />
-                <img className='logosearch' src='./assets/img/search-icon.png'/>
+            <div className='img-container'>
+                <img className='logo' src='./assets/img/logo.png'/>
             </div>
-            <div className='faq'>
-                <img className='logofaq' src='./assets/img/faq.png'/>
-                <p className='faqq'>FAQ</p>
-            </div>
-            <div className='loginbutton'>
-                <button className='login'>
-                    <p className='loginn'>LOGIN</p>
-                </button>
+            <div className='flexitem'>
+                <div className='searchbar'>
+                    <input className='search' type="text" placeholder='Search here . . .' />
+                    <img className='logosearch' src='./assets/img/search-icon.png'/>
+                </div>
+                <div className='faq'>
+                    <img className='logofaq' src='./assets/img/faq.png'/>
+                </div>
+                <div className='loginbutton'>
+                    <button className='login'>
+                        <p className='loginn'>LOGIN</p>
+                    </button>
+                </div>
             </div>
         </div>
     </div>
