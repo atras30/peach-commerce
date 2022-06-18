1ea42b61047d9f4f2fbe5d9e8300818c93e019d0
kelshaaira <kelshairaa@gmail.com>
2022-06-18 15:09:01 +0700

    landing page header doang

--- frontend/src/components/LandingPage.jsx
+++ frontend/src/components/LandingPage.jsx
@@ -3,10 +3,23 @@ import React from 'react'
 export default function LandingPage() {
   return (
     <div>
-        <link href='./assets/css/landingPage.css'/>
+        <link rel="stylesheet" href='./assets/css/landingPage.css'/>
 
         <div className='header'>
-            
+            <img className='logo' src='./assets/img/logo.png'/>
+            <div className='searchbar'>
+                <input className='search' type="text" placeholder='Search here . . .' />
+                <img className='logosearch' src='./assets/img/search-icon.png'/>
+            </div>
+            <div className='faq'>
+                <img className='logofaq' src='./assets/img/faq.png'/>
+                <p className='faqq'>FAQ</p>
+            </div>
+            <div className='loginbutton'>
+                <button className='login'>
+                    <p className='loginn'>LOGIN</p>
+                </button>
+            </div>
         </div>
     </div>
   )
