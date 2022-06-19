3840dda436d108172c71f0a97e5ec218b53fd944
kelshaaira <kelshairaa@gmail.com>
2022-06-19 18:29:55 +0700

    benerin kekurangan

--- frontend/src/components/LandingPage.jsx
+++ frontend/src/components/LandingPage.jsx
@@ -1,59 +1,93 @@
-import React from 'react';
+import React, {useRef} from 'react';
+import {Link} from 'react-router-dom';
+import Axios from 'axios';
 
 export default function LandingPage() {
+    const inputSearch = useRef(null);
+    
+    // Filter produk berdasarkan input user (search input)
+    function handleSearch() {
+        let text = inputSearch.current.value;
+
+        console.log(text);
+    }
+
+    // Fetch data API Product
+    function getAllProducts() {
+        Axios.get('http://127.0.0.1:8000/api/products')
+        .then(function (response) {
+            // handle success
+            console.log(response);
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        });
+    }
+    
   return (
     <div className='landing-page'>
         <link rel="stylesheet" href='./assets/css/landingPage.css'/>
 
         <div className='header'>
             <div className='img-container'>
-                <img className='logo' src='./assets/img/logo.png' alt="Peach Commerce Logo"/>
+                <Link to="/" className='img-logo-wrapper'>
+                    <img className='logo' src='./assets/img/logo.png' alt="Peach Commerce Logo"/>  
+                </Link>
             </div>
             <div className='flexitem'>
                 <div className='searchbar'>
-                    <input className='search' type="text" placeholder='Search here . . .' />
-                    <img className='logosearch' src='./assets/img/search-icon.png' alt="Search Icon"/>
+                    <input ref={inputSearch} className='search' type="text" placeholder='Search here . . .' />
+                    <img onClick={handleSearch} className='logosearch' src='./assets/img/search-icon.png' alt="Search Icon"/>
                 </div>
                 <div className='faq'>
-                    <img className='logofaq' src='./assets/img/faq.png' alt="Frequently Asked Questions"/>
+                    <Link to="/FAQ">
+                        <img className='logofaq' src='./assets/img/faq.png' alt="Frequently Asked Questions"/>
+                    </Link>
                 </div>
                 <div className='loginbutton'>
-                    <button className='login'>
-                        <p className='loginn'>LOGIN</p>
-                    </button>
+                    <Link to="/Login">
+                        <button className='login'>
+                            <p className='loginn'>LOGIN</p>
+                        </button>
+                    </Link>
                 </div>
             </div>
         </div>
 
         <div className='category'>
-            <p className='categories'>Category 1</p>
-            <p className='categories'>Category 2</p>
-            <p className='categories'>Category 3</p>
-            <p className='categories'>Category 4</p>
-            <p className='categories'>Category 5</p>
-            <p className='categories'>Category 6</p>
-            <p className='categories'>Category 7</p>
+            <p className='categories'>Pakaian</p>
+            <p className='categories'>Makanan</p>
+            <p className='categories'>Elektronik</p>
+            <p className='categories'>Komputer & Aksesoris</p>
+            <p className='categories'>Kesehatan</p>
+            <p className='categories'>Perawatan</p>
         </div>
 
+        <div className='productContainer'>
+            <button onClick={getAllProducts}>get all products</button>
+        </div>
+        
         <div className='footer'>
             <div className='footer1'>
                 <img className='logofooter' src='./assets/img/logo.png' alt="Peach Commerce Logo"/>
-                <p className='about'>ABOUT PEACH</p>
+                <a href="#" className='about'>ABOUT PEACH</a>
             </div>
+        
             <div className='footer2'>
                 <p className='follow'>FOLLOW US:</p> <br/>
-                <p className='sosmed'>INSTAGRAM</p>
-                <p className='sosmed'>TWITTER</p>
-                <p className='sosmed'>TIKTOK</p>
+                <a href="#" className='sosmed'>INSTAGRAM</a>
+                <a href="#" className='sosmed'>TWITTER</a>
+                <a href="#" className='sosmed'>TIKTOK</a>
             </div>
             <div className='footer3'>
                 <p className='download'>DOWNLOAD PEACH HERE:</p>
-                <img className='barcode' src='./assets/img/barcode.png' alt="Barcode"/>
-            </div>
-            <div className='link'>
-                <div className='downloadlink'>
-                    <p>PLAYSTORE</p>
-                    <p>APPSTORE</p>
+                <div className='footer4'>
+                    <img className='barcode' src='./assets/img/barcode.png' alt="Barcode"/>
+                    <div className='footer5'>
+                        <a href="#">PLAYSTORE</a>
+                        <a href="#">APPSTORE</a>
+                    </div>
                 </div>
             </div>
         </div>
