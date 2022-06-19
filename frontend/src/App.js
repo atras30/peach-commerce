6cc2f68f22eb6b59b93bff176fd355b17eb5923e
atras30 <atrasshalhan@gmail.com>
2022-06-19 10:16:30 +0700

    alt attributes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import LandingPage from "./components/LandingPage";
 
 function App() {
   return (
-   <div>
+   <div className="app">
     <LandingPage/>
    </div>
   );
--- frontend/src/components/LandingPage.jsx
+++ frontend/src/components/LandingPage.jsx
@@ -2,20 +2,20 @@ import React from 'react';
 
 export default function LandingPage() {
   return (
-    <div>
+    <div className='landing-page'>
         <link rel="stylesheet" href='./assets/css/landingPage.css'/>
 
         <div className='header'>
             <div className='img-container'>
-                <img className='logo' src='./assets/img/logo.png'/>
+                <img className='logo' src='./assets/img/logo.png' alt="Peach Commerce Logo"/>
             </div>
             <div className='flexitem'>
                 <div className='searchbar'>
                     <input className='search' type="text" placeholder='Search here . . .' />
-                    <img className='logosearch' src='./assets/img/search-icon.png'/>
+                    <img className='logosearch' src='./assets/img/search-icon.png' alt="Search Icon"/>
                 </div>
                 <div className='faq'>
-                    <img className='logofaq' src='./assets/img/faq.png'/>
+                    <img className='logofaq' src='./assets/img/faq.png' alt="Frequently Asked Questions"/>
                 </div>
                 <div className='loginbutton'>
                     <button className='login'>
@@ -33,14 +33,11 @@ export default function LandingPage() {
             <p className='categories'>Category 5</p>
             <p className='categories'>Category 6</p>
             <p className='categories'>Category 7</p>
-            <p className='categories'>Category 8</p>
-            <p className='categories'>Category 9</p>
-            <p className='categories'>Category 10</p>
         </div>
 
         <div className='footer'>
             <div className='footer1'>
-                <img className='logofooter' src='./assets/img/logo.png'/>
+                <img className='logofooter' src='./assets/img/logo.png' alt="Peach Commerce Logo"/>
                 <p className='about'>ABOUT PEACH</p>
             </div>
             <div className='footer2'>
@@ -51,7 +48,7 @@ export default function LandingPage() {
             </div>
             <div className='footer3'>
                 <p className='download'>DOWNLOAD PEACH HERE:</p>
-                <img className='barcode' src='./assets/img/barcode.png'/>
+                <img className='barcode' src='./assets/img/barcode.png' alt="Barcode"/>
             </div>
             <div className='link'>
                 <div className='downloadlink'>
@@ -59,7 +56,7 @@ export default function LandingPage() {
                     <p>APPSTORE</p>
                 </div>
             </div>
-            </div>
         </div>
+    </div>
   )
 }
