f317fe566bd455659a11066a51e90ade35d6c60d
atras30 <atrasshalhan@gmail.com>
2022-06-24 18:41:59 +0700

    tambahin route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import LandingPage from "./components/LandingPage";
+import AddProductPage from "./components/AddProductPage";
+import Profile from "./components/Profile";
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom"; 
 
 function App() {
@@ -7,6 +9,8 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage/>}></Route>
+        <Route path="/product/add-product" element={<AddProductPage/>}></Route>
+        <Route path="/user/profile" element={<Profile/>}></Route>
       </Routes>
     </Router>
   );
---
+++ frontend/src/components/AddProductPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function AddProductPage() {
+  return (
+    <div>AddProductPage</div>
+  )
+}
