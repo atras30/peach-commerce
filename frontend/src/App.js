6e0251eb614f8405fb9b1fcc87afdb9d614becbf
atras30 <atrasshalhan@gmail.com>
2022-07-04 16:52:04 +0700

    adding react-helmet

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,20 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import LandingPage from "./components/LandingPage";
 import AddProductPage from "./components/AddProductPage";
 import Profile from "./components/Profile";
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import {Helmet} from "react-helmet";
 import "./assets/css/landingPage.css";
 
 function App() {
   const [authenticatedUser, setAuthenticatedUser] = useState(null);
-  
+
   return (
     <>
-      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossOrigin="anonymous" />
+      <Helmet>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossOrigin="anonymous" />
+        <script defer type="text/javascript" src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+      </Helmet>
 
       <Router>
         <Routes>
